2f63f20b0547315d2f23306eccb7e098fdf7df59
catalin-sbora <catalin.sbora@gmail.com>
2025-03-27 11:04:04 +0200

    Update Program.cs

--- RazorTestApp/Program.cs
+++ RazorTestApp/Program.cs
@@ -7,8 +7,9 @@ namespace RazorTestApp
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddRazorPages();
-            asdasdasd asdasdasd;
+            builder.Services
+                .AddRazorPages();
+           
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
